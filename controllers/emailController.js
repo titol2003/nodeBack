d90c7083cae4b92d3acaa89bf4e1e47d968a8cb3
titol2003 <titolospina@gmail.com>
2023-08-15 19:53:57 -0500

    Punto de version

--- controllers/emailController.js
+++ controllers/emailController.js
@@ -1,4 +1,6 @@
 import { Resend } from 'resend';
+import Agente from '../models/agente.js';
+
 const RESEND_API_KEY = 're_SQFocMJw_2wBJ5v3JbASmhMnHCiw7Z1AQ'; 
 
 const resend = new Resend(RESEND_API_KEY)
@@ -6,25 +8,32 @@ const resend = new Resend(RESEND_API_KEY)
 export const createEmail= async (req, res) => {
     try {
 
-        const formData = req.body.formData; // Supongo que en tu solicitud AJAX envías el formData completo
-        const htmlContent = `
-        <h2>Detalles del formulario:</h2>
-        <p><strong>Nombre:</strong> ${formData.nombre}</p>
-        <p><strong>Email:</strong> ${formData.email}</p>
-        <p><strong>Número Telefónico:</strong> ${formData.telefono}</p>
-        <p><strong>Comentarios:</strong> ${formData.comentarios}</p>
-        `;
-       
-        const data = await resend.emails.send({
-          from: "Acme <onboarding@resend.dev>",
-          to: ['titolospina@gmail.com'/* req.body.correo*/],
-          subject: "Contactenos, MultiHogar-MH",
-          html: htmlContent,
-        });
-        res.status(200).json({ data });
-      } catch (error) {
-        console.log(error)
+        const formData = req.body.formData;
+
+        const agente = await Agente.findOne({});
+
+        if (agente) {
+            const htmlContent = `
+              <h2>Detalles del formulario:</h2>
+              <p><strong>Nombre:</strong> ${formData.nombre}</p>
+              <p><strong>Email:</strong> ${formData.email}</p>
+              <p><strong>Número Telefónico:</strong> ${formData.telefono}</p>
+              <p><strong>Comentarios:</strong> ${formData.comentarios}</p>
+            `;
+      
+            const data = await resend.emails.send({
+              from: "Acme <onboarding@resend.dev>",
+              to: [agente.email], // Usamos el correo del agente obtenido del modelo
+              subject: "Contactenos, MultiHogar-MH",
+              html: htmlContent,
+            });
+      
+            res.status(200).json({ data });
+        } else {
+        res.status(404).json({ error: "Agente no encontrado" });
+        }
+    } catch (error) {
+        console.log(error);
         res.status(500).json({ error });
-        
-      }
-  };
+    }
+};
